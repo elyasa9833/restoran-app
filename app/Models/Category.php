a9b070cd5b9d705c3274e254eb79cbfc93999b0d
elyasa9833 <aelyasa09@gmail.com>
2023-09-08 20:20:24 +0700

    change controller name

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class KategoriMakanan extends Model
+class Category extends Model
 {
-    // use HasFactory;
-    // protected $fillable  ['name'];
+    use HasFactory;
 }
