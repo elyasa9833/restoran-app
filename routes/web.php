f90c8a293674c73b2dfd3f2be2aef197b1e47e90
elyasa9833 <aelyasa09@gmail.com>
2023-09-10 10:09:48 +0700

    Added CategoryController, model, migrate, route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\CategoryController;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/category/create', [CategoryController::class, 'create']);
+Route::resource('category', 'App\Http\Controllers\CategoryController');
